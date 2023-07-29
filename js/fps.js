0b95018732df046e0e1b40157448d3654079c7f9
tabotef134 <tabotef134@outlook.com>
2023-07-29 12:38:40 +0800

    Site updated: 2023-07-29 12:38:37

--- js/fps.js
+++ js/fps.js
@@ -153,3 +153,74 @@ function toggleRightside(){
 }
 
 document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JvvRpNWGgKG94NZL\",ck:\"JvvRpNWGgKG94NZL\"});</script>")
+
+
+
+if(window.location.href == 'http://localhost:4000/about/'||window.location.href == 'https://jiyu134.top/about'){
+    Snackbar.show({ actionText: '关闭本弹窗',text: '可以动一动你们的小手给我一键三连吗？',backgroundColor: '#01f0ef',actionTextColor: '#0df016',pos: 'top-center',duration: '3000' });
+  }
+
+if(window.location.href == 'http://localhost:4000/zzq/'||window.location.href == 'https://jiyu134.top/zzq'){
+    Snackbar.show({ actionText: '关闭本弹窗',text: '合理消费享受健康人生',backgroundColor: '#01f0ef',actionTextColor: '#0df016',pos: 'top-center',duration: '7000' });
+  }
+
+function browserTC() {
+    btf.snackbarShow("");
+    Snackbar.show({
+        text: '浏览器版本较低，网站样式可能错乱!',
+        actionText: '关闭',
+        duration: '10000',
+        pos: 'top-center'
+    });
+}
+function browserVersion() {
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器
+    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //Edge浏览器
+    var isFirefox = userAgent.indexOf("Firefox") > -1; //Firefox浏览器
+    var isOpera = userAgent.indexOf("Opera")>-1 || userAgent.indexOf("OPR")>-1 ; //Opera浏览器
+    var isChrome = userAgent.indexOf("Chrome")>-1 && userAgent.indexOf("Safari")>-1 && userAgent.indexOf("Edge")==-1 && userAgent.indexOf("OPR")==-1; //Chrome浏览器
+    var isSafari = userAgent.indexOf("Safari")>-1 && userAgent.indexOf("Chrome")==-1 && userAgent.indexOf("Edge")==-1 && userAgent.indexOf("OPR")==-1; //Safari浏览器
+    if(isEdge) {
+        if(userAgent.split('Edge/')[1].split('.')[0]<90){
+            browserTC()
+        }
+    } else if(isFirefox) {
+        if(userAgent.split('Firefox/')[1].split('.')[0]<90){
+            browserTC()
+        }
+    } else if(isOpera) {
+        if(userAgent.split('OPR/')[1].split('.')[0]<80){
+            browserTC()
+        }
+    } else if(isChrome) {
+        if(userAgent.split('Chrome/')[1].split('.')[0]<90){
+            browserTC()
+        }
+    } else if(isSafari) {
+        //不知道Safari哪个版本是该淘汰的老旧版本
+    }
+}
+//2022-10-29修正了一个错误：过期时间应使用toGMTString()，而不是toUTCString()，否则实际过期时间在中国差了8小时
+function setCookies(obj, limitTime) {
+	let data = new Date(new Date().getTime() + limitTime * 24 * 60 * 60 * 1000).toGMTString()
+	for (let i in obj) {
+		document.cookie = i + '=' + obj[i] + ';expires=' + data
+	}
+}
+function getCookie(name) {
+	var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+	if (arr = document.cookie.match(reg))
+		return unescape(arr[2]);
+	else
+		return null;
+}
+if(getCookie('browsertc')!=1){
+    setCookies({
+        browsertc: 1,
+    }, 1);
+    browserVersion();
+}
+
+
