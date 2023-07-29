bbaf954eebf0cb442a0405d67603d9237ba98028
tabotef134 <tabotef134@outlook.com>
2023-07-29 13:48:32 +0800

    Site updated: 2023-07-29 13:48:29

--- js/utils.js
+++ js/utils.js
@@ -616,7 +616,7 @@ const anzhiyu = {
     let evt = document.createEvent("HTMLEvents");
     evt.initEvent("input", true, true);
     input.value =
-      "昵称（请勿包含博客等字样）：\n网站地址（要求博客地址，请勿提交个人主页）：\n头像图片url（请提供尽可能清晰的图片，我会上传到我自己的图床）：\n描述：\n站点截图（可选）：\n";
+      "昵称（请勿包含博客等字样）：\n网站地址（要求博客地址，请勿提交个人主页）：\n头像图片url（请提供尽可能清晰的图片）：\n描述：\n";
     input.dispatchEvent(evt);
     input.focus();
     input.setSelectionRange(-1, -1);
@@ -836,7 +836,7 @@ const anzhiyu = {
       return;
     }
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "8152976493";
+    const userId = "8802438608";
     const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
